bd24b41bed77f1abf297eee012ea0d045734591d
kratss <krats@noreply.codeberg.org>
2024-10-26 14:28:02 +0200

    Fish update

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-abbr --add vi --position command vim
-
-#abbr --add n --position command nnn
+abbr --add nv --position command nv

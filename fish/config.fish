551ac8014660255ce15231c4f80b08388638ddda
kratss <krats@noreply.codeberg.org>
2024-11-23 13:35:42 +0100

    Nvim fix

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-abbr --add nv --position command nv
+abbr --add vi --position command vim
+
+abbr --add n --position command nnn
+
